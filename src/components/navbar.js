536d9eead8c863fa0edd72d0ec90e8e06c0c3880
grace-kamau254 <kamaugraciie@gmail.com>
2025-03-07 21:35:02 +0300

    added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React,{useState}from 'react'
+import {link} from 'react-router-dom'
+fuction Navbar(){
+    <>
+<nav className="navbar">
+    <div className="navbar-container">
+<link to="/a" className="navbar-logo" ></link>
+    </div>
+</nav>
+
+    </>
+}
+export default navbar:
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
 import { StrictMode } from 'react'
+import Navbar from <div className="" />
+<source />
+<component />
+<navbar className="js"></navbar>
+<navbar className="js"></navbar>
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'

7d08b00edc242947ae8c02a9c9e754ce5a98fabf
grace-kamau254 <kamaugraciie@gmail.com>
2025-03-09 22:03:32 +0300

    fixed a bug

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import React,{useState}from 'react'
 import {link} from 'react-router-dom'
-fuction Navbar(){
+function Navbar(){
     <>
 <nav className="navbar">
     <a href="/">Home</a>
@@ -12,5 +12,6 @@ fuction Navbar(){
     </>
 }
 export default navbar:
+
 
 
